59a51a38240671caed2689c3707c8895377c1a48
Ajibola <52489772+AjibolaMatthew1@users.noreply.github.com>
2022-03-11 15:08:48 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Return - 0 Always(Success)
- */
+ * main - Prints the alphabetic in lower and upper case
+ *
+ * Return: Always (Success)
+ **/
 int main(void)
 {
-	char c;
-	
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		putchar(c);
-	}
-	for (c = 'A'; c <= 'Z'; c++)
-	{
-		putchar(c);
-	}
-	putchar('\n');
-	return (0);
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			putchar(c);
+		}
+
+		for (c = 'A'; c <= 'Z'; c++)
+		{
+			putchar(c);
+		}
+
+		putchar('\n');
+
+		return (0);
 }
+© 2022 GitHub, Inc.
+Terms
+Pri
