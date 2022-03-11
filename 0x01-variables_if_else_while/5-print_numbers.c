3814d086340247621727bb69a960d103365db127
Ajibola <52489772+AjibolaMatthew1@users.noreply.github.com>
2022-03-11 16:13:38 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
+
 /**
- * main - Entry point 
- * Returns 0 Always(Success)
- */
+ * main - Prints the numbers since 0 to 9
+ *
+ * Return: Always (Success)
+ **/
 int main(void)
 {
-	char i;
-	
-	for (i = '0'; i <= '9'; i++)
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
 	{
-		putchar(i);
+		putchar(n);
 	}
-	
+
 	putchar('\n');
-	
+
 	return (0);
 }
