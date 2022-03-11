ee48cd8ebd98c552d47643960d98c07e17deba0c
Ajibola <52489772+AjibolaMatthew1@users.noreply.github.com>
2022-03-11 16:03:55 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - Entry Point 
  * Return - Always 0
  */
 int main(void) 
 {
-    char c;
+	char c;
 
-    for (c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		if (c != 'e' && c != 'q')
 		{
