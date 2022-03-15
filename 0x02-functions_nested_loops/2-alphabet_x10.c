605d132c739135768299dff95c4d0ef7f6216f97
Ajibola <davidajibolatimilehin@gmail.com>
2022-03-15 13:01:09 +0100

    added file

---
+++ 0x02-functions_nested_loops/2-alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - prints the alphabet 10 times
+ *
+ *Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+
+	while (i <= 10)
+	{
+		while (c <= 'z')
+			{
+				_putchar(c);
+				c++;
+			}
+		_putchar('\n');
+	}
+}
+
