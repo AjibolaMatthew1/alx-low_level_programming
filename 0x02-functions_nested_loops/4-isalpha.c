01d51c0a8dcec0522861d5cfee8cdd87c0e64457
Ajibola <52489772+AjibolaMatthew1@users.noreply.github.com>
2022-03-15 14:32:29 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,10 +2,10 @@
 
 /**
  *_isalpha: returns if string is a letter
- *
+ * @n: the int to check
  *Return: 1 if c is letter and 0 otherwise
  */
 int _isalpha(int c)
 {
-	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
 }
