b111dde5c565831ca57371896fb34c458f09c079
Ajibola <52489772+AjibolaMatthew1@users.noreply.github.com>
2022-03-18 21:39:41 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if ((i % 5 == 0) && (i % 100 != 0))
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else if ((i % 5 == 0) && (i % 100 == 0))
 		{
@@ -25,7 +25,7 @@ int main(void)
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		printf("%d", i);
 	}
