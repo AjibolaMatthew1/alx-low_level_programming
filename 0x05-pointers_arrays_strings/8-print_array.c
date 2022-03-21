4b4f5b4297cdceaea0bd26e34b52e9fb357683a9
Ajibola <52489772+AjibolaMatthew1@users.noreply.github.com>
2022-03-21 22:16:15 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,13 +12,14 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i < n - 1)
+		if (i == 0)
 		{
-			printf("%d, ", a[i]);
+			printf("%d", a[i]);
 		}
 		else
 		{
-			printf("%d\n", a[i]);
+			printf(", %d", a[i]);
 		}
 	}
+	printf("\n");
 }
