b71f7b33b091d8cac584e6bdaec27fb6eb6c87af
Ajibola <52489772+AjibolaMatthew1@users.noreply.github.com>
2022-03-21 22:00:55 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,24 @@
 #include "main.h"
 
 /**
- * puts - prints every other character starting from first
+ * puts2 - prints every other character starting from first
  * @str: string to be processed
  */
 void puts2(char *str)
 {
-	int i;
+	int len, i;
 
-	_putchar(str[0]);
-	for (i = 0; str[i] != '\0'; i++)
+	len = 0;
+
+	while (str[len] != '\0')
 	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
+		len++;
 	}
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
 	_putchar('\n');
 }
