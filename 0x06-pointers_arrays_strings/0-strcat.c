ecdb7de355154742229d2e63cf0c9f5cdfe8cce6
Ajibola <davidajibolatimilehin@gmail.com>
2022-03-23 10:57:02 +0100

    New additions

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcat - this functions concatenates strings together.
+ * @dest: this is the string that would be concatenated to.
+ * @src: this is the string that would be added to dest
+ * Return: char
+ */
+char *_strcat(*dest, char *src)
+{
+	int i, j = 0, k, l;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	l = i + 1;
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+	for (k = 0; k <= j + 1; k++)
+	{
+		while (l > i && l < (i + j + 1))
+		{
+			dest[l] = src[k];
+
+		}
+	}
+	return (*dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+## More on array and strings with pointers. 
