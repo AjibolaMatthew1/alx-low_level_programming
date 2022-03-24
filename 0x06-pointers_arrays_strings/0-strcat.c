6e9197928b8981930d49450043af20369b03f457
Ajibola <52489772+AjibolaMatthew1@users.noreply.github.com>
2022-03-24 11:57:20 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,27 +6,24 @@
  * @src: this is the string that would be added to dest
  * Return: char
  */
-char *_strcat(*dest, char *src)
+char *_strcat(char *dest, char *src)
 {
-	int i, j = 0, k, l;
+	int i, j;
 
 	i = 0;
+	j = 0;
+
 	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
 	{
+		dest[i] = src[j];
+		j++;
 		i++;
 	}
-	l = i + 1;
-	while (dest[j] != '\0')
-	{
-		j++;
-	}
-	for (k = 0; k <= j + 1; k++)
-	{
-		while (l > i && l < (i + j + 1))
-		{
-			dest[l] = src[k];
 
-		}
-	}
+	dest[i] = '\0';
+
 	return (dest);
 }
