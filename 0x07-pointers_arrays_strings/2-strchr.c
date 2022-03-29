de0e1c3f49e6d2b2f49d7136bae1e5afa811da5d
Ajibola <52489772+AjibolaMatthew1@users.noreply.github.com>
2022-03-29 17:14:13 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,7 +2,6 @@
 
 /**
  * _strchr - returns a pointer to the first occurence
- * of character c in the string s
  * @s: string to be checked
  * @c: the character to be searched for
  * Return: a pointer
@@ -10,7 +9,7 @@
 char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
-	
+
 	for (; *(s + i) != '\0'; i++)
 	{
 		if (*(s + i) == c)
