e98caefeb83f369accfba23f1ba49f686f7178ca
Ajibola <52489772+AjibolaMatthew1@users.noreply.github.com>
2022-03-30 17:48:36 +0100

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,12 +12,9 @@ int _pow_recursion(int x, int y)
 	{
 		return (1);
 	}
-	else if (y < 0)
+	if (y < 0)
 	{
 		return (-1);
 	}
-	else
-	{
-		return (x * _pow_recursion(x, y--));
-	}
+	return (x * _pow_recursion(x, y - 1));
 }
