0785bb93e4991b4dc7140632fd41b8185033c353
Ajibola <52489772+AjibolaMatthew1@users.noreply.github.com>
2022-03-30 14:52:49 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,14 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (*(s + i) == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	_putchar(*(s + i));
-	i++;
-	return (_puts_recursion((s + i)));
+	_putchar(*s);
+	_puts_recursion((s + 1));
 }
