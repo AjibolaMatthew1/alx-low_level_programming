8eeb3005233a9f4853e386d928ec46ccc335615a
Ajibola <davidajibolatimilehin@gmail.com>
2022-04-01 21:37:36 +0100

    added whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc - argument counter
+ * @argv[] - argument vector
+ * Return - 0 (success)
+ */
+int main(int argc, char* argv[])
+{
+	printf("%s \n", argv[0])
+}
