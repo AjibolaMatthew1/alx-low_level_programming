45122851f3b710d38dd8d75a15e99cfd9a263976
AjibolaMatthew1 <davidajibolatimilehin@gmail.com>
2022-04-01 14:35:34 -0700

    added 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - pritns number of arguments in the command line
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	argument1 = argv[0];
+	return (0);
+}
