30b120a14a1457bda76f3b17994c9b6bfcea55df
Ajibola <52489772+AjibolaMatthew1@users.noreply.github.com>
2022-04-01 23:56:13 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,6 +11,7 @@
 int numchecker(char *s)
 {
 	int i;
+	
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] < '0' || s[i] > '9')
@@ -29,20 +30,21 @@ int numchecker(char *s)
 int main(int argc, char **argv)
 {
 	int sum, i;
+	
 	sum = 0;
 	for (i = 1; i < argc; i++)
 	{
 		if (numchecker(argv[i]) == 0)
-	{
-	printf("Error\n");
-	return (1);
-	}
-	if (atoi(argv[i]) <= 0)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	sum += atoi(argv[i]);
+		{
+			printf("Error\n");
+			return (1);
+		}
+		if (atoi(argv[i]) <= 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
