a97f0e72ea10eb7671ac5ce36130c633b46196c6
Ajibola <davidajibolatimilehin@gmail.com>
2022-04-01 23:09:44 +0100

    added 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers from command line
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0 (success) and 1(failure)
+ */
+int main(int argc, char *argv[])
+{
+	if ((argc - 1) == 2)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]]))
+	}
+	else
+	{
+		printf("Error");
+		return (1);
+	}
+	return (0);
+}
