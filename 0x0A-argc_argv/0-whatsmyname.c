417c781b6e2af287295eff645ed1d9a774b7f340
Ajibola <52489772+AjibolaMatthew1@users.noreply.github.com>
2022-04-01 22:28:31 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,15 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		if (i == 0)
-		{
-			printf("%s \n", argv[0]);
-			break;
-		}
-	}
+	printf("%s\n", argv[0]);
+	argc = argc;
 	return (0);
 }
