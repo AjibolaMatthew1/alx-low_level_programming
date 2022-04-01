685d813e0b3c498b7a647f8d07ba9d7c7dcdcc9a
Ajibola <52489772+AjibolaMatthew1@users.noreply.github.com>
2022-04-01 23:52:36 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,32 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
- * main - adds positive numbers
- * @argc: argument counter
- * @argv: argument vector
- * Return: 0(success) , 1 (failure)
- */
-int main(int argc, char *argv[])
+* numchecker - verifies if string has only numbers
+* @s: string to check
+* Description: return 1 if only numbers in string, 0 if not
+* Return: 1 if only numbers, 0 if not
+**/
+int numchecker(char *s)
 {
-	int i, sum;
-
-	sum = 0;
-	if ((argc) == 1)
+	int i;
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		printf("0\n");
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
 	}
+	return (1);
+}
+
+/**
+* main - find least amount of coins needed for change
+* @argc: number of parameters entered
+* @argv: strings entered including file name
+* Description: prints the minimum number of coins to make change
+* Return: zero
+**/
+int main(int argc, char **argv)
+{
+	int sum, i;
+	sum = 0;
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) >= '0' && atoi(argv[i]) <= '9')
-		{
-			sum += atoi(argv[i]);
-		}
-		else if (atoi(argv[i]) < '0' || atoi(argv[i]) > '9')
-		{
-			printf("Error\n");
-			return (1);
-		}
+		if (numchecker(argv[i]) == 0)
+	{
+	printf("Error\n");
+	return (1);
+	}
+	if (atoi(argv[i]) <= 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
