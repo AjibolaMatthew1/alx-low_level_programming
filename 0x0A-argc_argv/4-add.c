3152fecd400ba6a634e6d239401490d070755c5d
Ajibola <52489772+AjibolaMatthew1@users.noreply.github.com>
2022-04-01 23:44:22 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 	}
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) >= '0' || atoi(argv[i]) <= '9')
+		if (atoi(argv[i]) >= '0' && atoi(argv[i]) <= '9')
 		{
 			sum += atoi(argv[i]);
 		}
