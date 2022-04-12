b7a32d46f0b51a6d1dcb2c51cb6a16bd02712522
Ajibola <52489772+AjibolaMatthew1@users.noreply.github.com>
2022-04-12 17:03:07 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * dog - tells us about dog
+ * name: name of the dog
+ * age: age of the dog
+ * owner: name of the owner of the dog
+ * Description: Gives us some information about a particular dog, the name, age and the owner's name.
+ */
 
-struct dog
+typedef struct dog dog_t
 {
     char *name;
     float age;
