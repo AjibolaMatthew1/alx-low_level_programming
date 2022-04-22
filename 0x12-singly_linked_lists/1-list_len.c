3376a90f96dee29f5d0bb82d20e15ccf1469c339
Ajibola <davidajibolatimilehin@gmail.com>
2022-04-22 23:40:45 +0100

    added 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - this returns the number of elements in a list
+ * @h: this is the list_t list to be checked
+ * Returns: the number of elements in the list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t counter;
+
+	counter = 0;
+	while (h)
+	{
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
