31680a91d4cc04307ecae410402b57d674165b3a
Ajibola <davidajibolatimilehin@gmail.com>
2022-04-23 21:05:24 +0100

    implemented a function that executes before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+void printBeforeMain(void) __attribute__((constructor));
+
+/**
+ * printBeforeMain - this function executes before main
+ * Return: Nothing
+ */
+void printBeforeMain(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
