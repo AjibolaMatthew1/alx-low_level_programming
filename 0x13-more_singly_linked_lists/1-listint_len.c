75f54856867a9cfddc7807268c3b7f2a64aee76a
Ajibola <davidajibolatimilehin@gmail.com>
2022-04-25 19:36:57 +0100

    added functionality to view length of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - this function returnds the number of element in a linked list
+ * @h: the head of the nodes
+ * Return: The number of elements in a linked list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t ele_count;
+
+	ele_count = 0;
+	while (h != NULL)
+	{
+		ele_count += 1;
+		h = h->next;
+	}
+	return ele_count;
+}
