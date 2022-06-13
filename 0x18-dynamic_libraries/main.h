b540a798fefaa243640b7edcc2ad2211b36e6169
Ajibola <52489772+AjibolaMatthew1@users.noreply.github.com>
2022-06-13 21:44:34 +0100

    Update and rename holberton.h to main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef _HOLBERTON_H
-#define _HOLBERTON_H
+#ifndef _MAIN_H
+#define _MAIN_H
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,4 +20,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif /* #ifndef _HOLBERTON_H */
+#endif /* #ifndef _MAIN_H */
